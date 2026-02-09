1e5783e107fd00de705355dab41d560d3d2c4c98
sungwhan.bae <sungwhan.bae@goodoc.co.kr>
2026-02-09 16:53:53 +0900

    chore: notion page fallback 추가

--- site.config.js
+++ site.config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: process.env.NOTION_PAGE_ID || "2ff5898a4b0581d1bc19d42da50ccc4f",
   },
 
   // plugin configuration (optional)
