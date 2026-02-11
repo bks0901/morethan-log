a7de04c1518ba157d99ac075a13ac314866d09fb
sungwhan.bae <sungwhan.bae@goodoc.co.kr>
2026-02-11 14:27:22 +0900

    fix: 이중대시로 인한 빌드에러 수정

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -5,5 +5,5 @@ export const getRecordMap = async (pageId?: string) => {
   const raw = pageId || process.env["NOTION_PAGE_ID"]
   if (!raw) throw new Error("pageId missing for getRecordMap")
   const api = new NotionAPI()
-  return api.getPage(idToUuid(raw))
+  return api.getPage(raw)
 }
